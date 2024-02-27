8c4ff9cacabbcc9e61254d5e753f2abdf44e1cb8
Yellow10041 <yellowvatral@gmail.com>
2024-02-27 10:32:54 +0200

    fix

--- src/components/img/index.tsx
+++ src/components/img/index.tsx
@@ -1,19 +1,32 @@
-import { FC } from "react";
-import clsx from "clsx";
-import styles from "./index.module.scss";
+import clsx from "clsx"
+import {FC} from "react"
+
+import styles from "./index.module.scss"
 
 interface IImg {
-  type?: "h" | "v";
-  size?: "small";
-  src: string;
+  type?: "h" | "v"
+  size?: "small"
+  src: string
+  float?: "left" | "right" | "none"
+  style?: {[key: string]: any}
 }
 
-export const Img: FC<IImg> = ({ type = "h", src, size = "" }) => {
+export const Img: FC<IImg> = ({
+  type = "h",
+  src,
+  size = "",
+  float = "none",
+  style,
+}) => {
   return (
     <img
       className={clsx(styles.Img, styles[type], styles[size])}
       src={src}
+      style={{
+        float: float,
+        ...style,
+      }}
       alt=""
     />
-  );
-};
+  )
+}
